3c7488479200d980c601425ebe43c60840854367
TC Chang <mocat.chang@gmail.com>
2013-07-09 15:34:47 +0000

    Read text from index.html

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
+var fs = require('fs');
+var data = fs.readFileSync('index.html');
+var buf = new Buffer(data);
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+    response.send(buf.toString());
+//  response.send('Hello World2!');
 });
 
 var port = process.env.PORT || 5000;
