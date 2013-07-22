48cae9b018e26a7978468c128b9db07065e6eafa
TC Chang <mocat.chang@gmail.com>
2013-07-22 14:26:50 +0000

    Add CSS and responsive design via Twitter Bootstrap

--- web.js
+++ web.js
@@ -1,5 +1,5 @@
 var fs = require('fs');
-var data = fs.readFileSync('index.html');
+var data = fs.readFileSync('index.html', 'utf8');
 var buf = new Buffer(data);
 
 var express = require('express');
@@ -7,11 +7,12 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
+//    console.log(buf.toString());
     response.send(buf.toString());
 //  response.send('Hello World2!');
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
